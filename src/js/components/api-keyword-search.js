d0e1a3a62f89ba78183c0a9a2f3882fe73344409
Vlad Horii <vladhorii@yandex.ru>
2021-06-16 12:38:13 +0300

    Добавлен функционирующий поиск фильмов

--- src/js/components/api-keyword-search.js
+++ src/js/components/api-keyword-search.js
@@ -1,37 +1,35 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export default class KeywordApiSearch {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        
-    }
-    
-    fetchMovies() {
-           const url = `${BASE_URL}/search/movie?api_key=0558fb418099b1d6ef291e53504aa0aa&query=${this.searchQuery}&page=${this.page}&language=en-US`;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
 
-            return fetch(url)
-             .then(response => response.json())
-                .then(({results}) => {
-                    this.incrementPage();
-                    return results;
-             });
-    }
+  fetchMovies() {
+    const url = `${BASE_URL}/search/movie?api_key=0558fb418099b1d6ef291e53504aa0aa&query=${this.searchQuery}&page=${this.page}&language=en-US`;
 
-    incrementPage() {
-        this.page += 1;
-    }
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        this.incrementPage();
+        return results;
+      });
+  }
 
-    // resetPage() {
-    //     this.page = 1;
-    // }
+  incrementPage() {
+    this.page += 1;
+  }
 
-    get query() {
-        return this.searchQuery;
-    }
+  // resetPage() {
+  //     this.page = 1;
+  // }
 
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
--- src/js/components/keyword-search.js
+++ src/js/components/keyword-search.js
@@ -1,39 +1,37 @@
-import KeywordApiSearch from './api-keyword-search';
-import moviesTpl from '../../../src/templates/movie-list.hbs';
-
+// import KeywordApiSearch from './api-keyword-search';
+import { MovieService } from '../api/api-movie-service';
+import { markupMovies } from '../markup/hero-markup';
 
 const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    moviesContainer: document.querySelector('.js-movies-container'),
-    // errorForm: document.querySelector('.visually-hidden'),
+  searchForm: document.querySelector('.js-search-form'),
+  moviesContainer: document.querySelector('.js-movies-container'),
+  // errorForm: document.querySelector('.visually-hidden'),
 };
-const keywordApiSearch = new KeywordApiSearch();
+// const keywordApiSearch = new KeywordApiSearch();
+const movieService = new MovieService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 
 function onSearch(e) {
-    e.preventDefault();
-    
-    keywordApiSearch.query = e.currentTarget.elements.query.value;
-    if (keywordApiSearch.query === '') {
-        return alert('Введи запрос');
-    }
-    // keywordApiSearch.resetPage();
-    keywordApiSearch.fetchMovies().then(results => {
-        clearMoviesContainer();
-        appendMoviesMarkup(results);
+  e.preventDefault();
 
-        // if (keywordApiSearch.fetchMovies() == 0) {
-        //     return errorForm;
+  movieService.query = e.currentTarget.elements.query.value;
+  if (movieService.query === '') {
+    return alert('Введи запрос');
+  }
+  movieService.resetPage();
 
-        // }
-    });
-}
+  movieService.fetchMoviesWithQuery().then(results => {
+    clearMoviesContainer();
+    markupMovies(results);
 
-function appendMoviesMarkup(results) {
-    refs.moviesContainer.insertAdjacentHTML('beforeend', moviesTpl(results));
+    // if (keywordApiSearch.fetchMovies() == 0) {
+    //     return errorForm;
+
+    // }
+  });
 }
 
 function clearMoviesContainer() {
-    refs.moviesContainer.innerHTML = '';
+  refs.moviesContainer.innerHTML = '';
 }
