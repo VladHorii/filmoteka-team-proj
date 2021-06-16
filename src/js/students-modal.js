9f317363da42482990b7311ba4fed1af933f2615
Nina Vasilieva <aninok92@gmail.com>
2021-06-16 22:48:46 +0300

    скрывает кнопку на вверх при открытии модалки + disableBodyScroll, enableBodyScroll

--- src/js/students-modal.js
+++ src/js/students-modal.js
@@ -1,3 +1,5 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
 const refs = {
   openStudentsModal: document.querySelector('[data-students-modal-open]'),
   closeStudentsModal: document.querySelector('[data-students-modal-close]'),
@@ -9,6 +11,14 @@ refs.closeStudentsModal.addEventListener('click', onClickStudentsModal);
 
 function onClickStudentsModal() {
   refs.studentsModal.classList.toggle('visually-hidden');
+  toggleScrollTopBtn();
+
+  if (!refs.studentsModal.classList.contains('visually-hidden')) {
+    disableBodyScroll(refs.closeStudentsModal);
+  }
+  if (refs.studentsModal.classList.contains('visually-hidden')) {
+    enableBodyScroll(refs.closeStudentsModal);
+  }
 }
 
 refs.studentsModal.addEventListener('click', onBackdropClick);
@@ -16,5 +26,11 @@ refs.studentsModal.addEventListener('click', onBackdropClick);
 function onBackdropClick(event) {
   if (event.currentTarget === event.target) {
     refs.studentsModal.classList.add('visually-hidden');
+    toggleScrollTopBtn();
+    enableBodyScroll(refs.closeStudentsModal);
   }
+}
+
+function toggleScrollTopBtn() {
+  mybutton.classList.toggle('visually-hidden');
 }
