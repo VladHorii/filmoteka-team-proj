f643dbc4bc56166a775dc0ae37d8b36b312b7546
Andres Link <andres.link@mail.ru>
2021-06-17 16:51:00 +0300

    minimised BSL functions import to 1 line, added BSL to students-modal

--- src/js/students-modal.js
+++ src/js/students-modal.js
@@ -1,11 +1,19 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
 const refs = {
   openStudentsModal: document.querySelector('[data-students-modal-open]'),
   closeStudentsModal: document.querySelector('[data-students-modal-close]'),
   studentsModal: document.querySelector('[data-students-modal]'),
 };
 
-refs.openStudentsModal.addEventListener('click', onClickStudentsModal);
-refs.closeStudentsModal.addEventListener('click', onClickStudentsModal);
+refs.openStudentsModal.addEventListener('click', function () {
+  onClickStudentsModal();
+  disableBodyScroll(refs.studentsModal);
+});
+refs.closeStudentsModal.addEventListener('click', function () {
+  onClickStudentsModal();
+  enableBodyScroll(refs.studentsModal);
+});
 
 function onClickStudentsModal() {
   refs.studentsModal.classList.toggle('visually-hidden');
@@ -16,5 +24,6 @@ refs.studentsModal.addEventListener('click', onBackdropClick);
 function onBackdropClick(event) {
   if (event.currentTarget === event.target) {
     refs.studentsModal.classList.add('visually-hidden');
+    enableBodyScroll(refs.studentsModal);
   }
 }
